cdcda04c203be5ce354b107b73284382edcf6171
boost666 <992043903@qq.com>
2018-12-11 10:57:14 +0800

    添加一个类

---
+++ src/main/java/com/User.java
@@ -0,0 +1,19 @@
+package com;
+
+public class User {
+	String userName ;
+	String passWrod;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWrod() {
+		return passWrod;
+	}
+	public void setPassWrod(String passWrod) {
+		this.passWrod = passWrod;
+	}
+	
+}
